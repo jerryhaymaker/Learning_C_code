aeac2c37837a9ebb87c0cd82b61838f51228105b
jerryhaymaker <75646690+jerryhaymaker@users.noreply.github.com>
2024-12-12 19:18:54 -0500

    Create meow.c

---
+++ meow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 3;
+    while (i > 0)
+    {
+        printf("meow\n");
+        i--;
+    }
+    // for (int i = 0; i < 3; i++)
+    //     printf('meow\n");
+    // while(true) endless loop
+}
